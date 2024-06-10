b9e8ccc1083dcf7d41e2f30cf54a91467911b3ac
Julien Brun <brunj7@users.noreply.github.com>
2024-06-09 21:57:51 -0700

    finishing episode 4

--- episode-4.R
+++ episode-4.R
@@ -15,13 +15,12 @@ DTM_HARV
 DSM_HARV <- rast("data/NEON-DS-Airborne-Remote-Sensing/HARV/DSM/HARV_dsmCrop.tif")
 DSM_HARV
 
-# Compute the difference
+# Transform in data frames for visualization
 DTM_HARV_df <- as.data.frame(DTM_HARV, xy=TRUE)
 DSM_HARV_df <- as.data.frame(DSM_HARV, xy=TRUE)
 
 
-#Let's have a look at it
-
+# Let's plot
 ggplot() +
   geom_raster(data = DTM_HARV_df , 
               aes(x = x, y = y, fill = HARV_dtmCrop)) +
@@ -35,14 +34,57 @@ ggplot() +
   scale_fill_gradientn(name = "Elevation", colors = terrain.colors(10)) + 
   coord_quickmap()
 
-# Compute the difference
+
+# Compute the difference using the simple way
 
 CHM_HARV <- DSM_HARV - DTM_HARV
-
+CHM_HARV
 CHM_HARV_df <- as.data.frame(CHM_HARV, xy = TRUE)
 
+#Check how it looks
 ggplot() +
   geom_raster(data = CHM_HARV_df , 
               aes(x = x, y = y, fill = HARV_dsmCrop)) + 
   scale_fill_gradientn(name = "Canopy Height", colors = terrain.colors(10)) + 
   coord_quickmap()
+
+# What is the distribution of canopy height?
+
+ggplot(CHM_HARV_df) +
+  geom_histogram(aes(HARV_dsmCrop))
+
+# Compute the difference using the advanced way
+
+# function in R
+
+# Function definition
+percentage <- function(x) {
+  x/100
+}
+
+# Using the function
+percentage(34)
+
+
+# defining our function
+CHM_ov_HARV <- lapp(sds(list(DSM_HARV, DTM_HARV)), 
+                    fun = function(r1, r2) { 
+                      r1 - r2
+                      })
+
+# Let's plot
+CHM_ov_HARV_df <- as.data.frame(CHM_ov_HARV, xy = TRUE)
+
+ggplot() +
+  geom_raster(data = CHM_ov_HARV_df, 
+              aes(x = x, y = y, fill = HARV_dsmCrop)) + 
+  scale_fill_gradientn(name = "Canopy Height", colors = terrain.colors(10)) + 
+  coord_quickmap()
+
+
+# Save our new raster
+writeRaster(CHM_ov_HARV, "CHM_HARV.tiff",
+            filetype="GTiff",
+            overwrite=TRUE,
+            NAflag=-9999)
+
